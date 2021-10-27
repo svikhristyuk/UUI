bd101b7007e269c0215fd435c186eacfbde29a33
rayetzki <rayetzkiillya@gmail.com>
2021-10-27 16:12:13 +0300

    [ReworkTable]: Virtualize table

--- app/src/sandbox/table-rework/ReworkTable.tsx
+++ app/src/sandbox/table-rework/ReworkTable.tsx
@@ -1,8 +1,9 @@
-import React, { CSSProperties, ReactNode, useMemo, useState } from "react";
-import { DataColumnProps } from "@epam/uui";
+import React, { CSSProperties, ReactNode, useMemo, useState, useRef, useCallback } from "react";
 import { Panel, Text } from "@epam/promo";
 import * as css from "./ReworkTable.scss";
+import { useVirtual } from 'react-virtual';
 import type { Product, FeatureClass } from '@epam/uui-docs';
+import type { DataColumnProps } from "@epam/uui";
 import { demoData } from './data';
 
 const TableCell = ({ children, style }: { children : ReactNode, style: CSSProperties }) => (
@@ -14,15 +15,20 @@ const TableCell = ({ children, style }: { children : ReactNode, style: CSSProper
 export function ReworkTable() {
     const [products] = useState<Product[]>(demoData as Product[]);
     const [tableShadows, setTableShadows] = useState({ top: false, bottom: false });
+    const bodyRef = useRef();
+
+    const { virtualItems, totalSize, scrollToIndex } = useVirtual({
+      size: products.length,
+      parentRef: bodyRef,
+      overscan: 50,
+      estimateSize: useCallback(() => 2, []),
+    });
 
     const updateScroll = (e: React.UIEvent<HTMLTableSectionElement, UIEvent>) => {
         const { scrollTop, scrollHeight, clientHeight } = e.currentTarget;
-
         if (!tableShadows.top && scrollTop > 0) setTableShadows({ ...tableShadows, top: true });
         else if (tableShadows.top && scrollTop === 0) setTableShadows({ ...tableShadows, top: true });
-
         const needsBottomShadow = scrollHeight - clientHeight > scrollTop;
-
         if (!tableShadows.bottom && needsBottomShadow) setTableShadows({ ...tableShadows, bottom: true });
         else if (tableShadows.bottom && !needsBottomShadow) setTableShadows({ ...tableShadows, bottom: false });
     };
@@ -87,36 +93,47 @@ export function ReworkTable() {
             <table className={css.Table}>
                 <thead className={css.Table__Header}>
                     <tr className={css.Table__Header__Row}>
-                        { productColumns.map(column => (
-                            <th key={column.key} className={css.Table__Header__Cell} style={{
-                                '--cell-min-width': column.minWidth ? `${column.minWidth}px` : undefined,
-                                '--cell-width': column.width ? `${column.width}px` : undefined,
-                                '--cell-grow': column.grow,
-                                '--cell-shrink': column.shrink
-                            } as CSSProperties}>
+                        {productColumns.map(column => (
+                            <th
+                                key={column.key}
+                                className={css.Table__Header__Cell}
+                                style={{
+                                    '--cell-min-width': column.minWidth ? `${column.minWidth}px` : undefined,
+                                    '--cell-width': column.width ? `${column.width}px` : undefined,
+                                    '--cell-grow': column.grow,
+                                    '--cell-shrink': column.shrink
+                                } as CSSProperties}>
                                 <Text size='36' font='sans-semibold' color='gray60'>{column.caption}</Text>
                             </th>
                         ))}
                     </tr>
-                    <div style={{ opacity: tableShadows.top ? 1 : 0 }} className='uui-scroll-shadow-top' />
+                    <span style={{ '--scroll-shadow-top': tableShadows.top ? 1 : 0 } as CSSProperties } className='uui-scroll-shadow-top' />
                 </thead>
-                <tbody className={css.Table__Body} onScroll={updateScroll}>
-                    {products.map(product => (
-                        <tr key={product.ModifiedDate.concat(product.Class).concat(String(product.ProductID))} className={css.Table__Body__Row}>
-                            {productCells.map(cell => (
-                                <TableCell style={ {
-                                    '--cell-min-width': cell.minWidth ? `${cell.minWidth}px` : undefined,
-                                    '--cell-width': cell.width ? `${cell.width}px` : undefined,
-                                    '--cell-grow': cell.grow,
-                                    '--cell-shrink': cell.shrink
-                                } as CSSProperties}>
-                                    {product[cell.key as keyof Product]}
-                                </TableCell>
-                            ))}
-                        </tr>
-                    ))}
+                <tbody
+                    style={{ '--body-height': `${totalSize}px` } as CSSProperties}
+                    ref={bodyRef}
+                    className={css.Table__Body}
+                    onScroll={updateScroll}
+                >
+                  {virtualItems.map(({ index, measureRef, start }) => (
+                    <tr key={index} style={{ '--body-row-offset': `${start}px` } as CSSProperties} ref={measureRef} className={css.Table__Body__Row}>
+                        {productCells.map(cell => (
+                            <TableCell style={ {
+                                '--cell-min-width': cell.minWidth ? `${cell.minWidth}px` : undefined,
+                                '--cell-width': cell.width ? `${cell.width}px` : undefined,
+                                '--cell-grow': cell.grow,
+                                '--cell-shrink': cell.shrink,
+                            } as CSSProperties}>
+                                {products[index][cell.key as keyof Product]}
+                            </TableCell>
+                        ))}
+                    </tr>
+                ))}
                 </tbody>
-                <div style={{ opacity: tableShadows.bottom ? 1 : 0 }} className='uui-scroll-shadow-bottom' />
+                <span
+                    style={{ '--scroll-shadow-bottom': tableShadows.bottom ? 1 : 0 } as CSSProperties}
+                    className='uui-scroll-shadow-bottom'
+                />
             </table>
         </Panel>
     );
